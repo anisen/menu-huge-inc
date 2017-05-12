7b219cddc7e2c7026141502f92f2cbd794fc7f62
“Anais <ananavelez@gmail.com>
2017-05-11 21:49:17 -0300

    primer desorden que no funciona

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,71 +1,15 @@
-nav {
-	top: 0;
-	left: 0;
-	width: 100%;
-	color: #fff;
-	height: 80px;
-	z-index: 1000;
-	margin-top: 0;
-	position: fixed;
-	padding-top: 0px;
-	background :none;
-	transition: all 0.6s cubic-beizer(0.7, 0.01, 0.3, 1);
-}
-
-
-/*Menu bar logo and menu bar style general*/
-.logo{
-	max-width: 30%;
-  display: block;
-  margin-right: auto;
-}
-.menu1{
-	position: absolute;
-	z-index: 2000;
-	top: 0;
-	right: 0;
-	height: 4px;
-	width: 24px;
-	text-align: right;
-	margin:2rem 2rem;
-	background: #ec008c;
-	transition: all 0.3s;
-}
-
-.menu2{
-	position: absolute;
-	z-index: 2000;
-	top: 10px;
-	right: 0;
-	height: 4px;
-	width: 24px;
-	text-align: right;
-	margin:2rem 2rem;
-	background: #ec008c;
-	transition: all 0.3s;
-}
-.menu3{
-	position: absolute;
-	z-index: 2000;
-	top: 20px;
-	right: 0;
-	height: 4px;
-	width: 24px;
-	text-align: right;
-	margin:2rem 2rem;
-	background: #ec008c;
-	transition: all 0.3s;
-}
-
-.logo{
-	left: 0;
-	top: -4px;
-	width: 70px;
-	height: 74px;
-	padding: 0.4% 2%;
-	text-align: left;
-	background-position: 50% 50%;
-	background-repeat: no-repeat;
-	background-image: url(../image/huge.png);
-	background-size: 70px, 10px;
-}
+document.getElementById("toggle").addEventListener("click", function(event){
+	event.preventDefault();
+	document.getElementById("nav-header").classList.toggle("open");
+	document.getElementById("body").classList.toggle("overflow-hidden");
+});
+var lastScrollTop =0;
+window.addEventListener("scroll", function(){
+	var currentScroll=window.pageYOffset || document.body.scrollTop;
+	if (currentScroll>lastScrollTop) {
+		console.log("down");
+	}else{
+		console.log("up");
+	}
+	/*lastScrollTop=currentScroll;*/
+});
